1b5e3572213f89fc46465377c3b8f2a191939292
bazarlink1122 <bazarlink1122@gmail.com>
2026-04-07 04:46:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-import main
-main.Xyz()
+import telebot
+from kivy.app import App
+from kivy.uix.label import Label
+
+# Aapka API Token
+TOKEN = '8675017242:AAFTFDKA_GzrII2qsiqratInvSq_4pfkArg'
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Assalam-o-Alaikum Saad Bhai! Aapka bot GitHub se ban kar tayyar ho chuka hai.")
+
+class MainApp(App):
+    def build(self):
+        # Jab app mobile mein khulegi to ye nazar ayega
+        return Label(text='Bot is Running Background...')
+
+if __name__ == '__main__':
+    # Bot ko background mein start karne ke liye
+    import threading
+    threading.Thread(target=bot.infinity_polling).start()
+    MainApp().run()
+  
